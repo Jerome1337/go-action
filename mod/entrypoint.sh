f85b0ffe8832148ee04b9c7c60e6cdc065fe6bc6
Jérôme Pogeant <p-jerome@hotmail.fr>
2020-04-13 16:25:35 +0200

    Update entrypoint.sh

--- mod/entrypoint.sh
+++ mod/entrypoint.sh
@@ -4,7 +4,7 @@ cd $GITHUB_WORKSPACE
 
 GO_MOD_OUPUT="$(go mod tidy -v 2>&1)"
 
-if $GO_MOD_OUPUT == *"unused"* ]; then
+if [ $GO_MOD_OUPUT == *"unused"* ]; then
   echo "${GO_MOD_OUPUT}"
 
   exit 1
